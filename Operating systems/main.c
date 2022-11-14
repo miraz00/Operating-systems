4af0dc31d1a55e261e6698a8bcdf0ef6c86309d8
miraz00 <mirazjnaina@gmail.com>
2022-11-14 13:18:43 +0530

    Initial Commit

---
+++ Operating systems/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Operating systems
+//
+//  Created by Miraz J Naina on 14/11/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
